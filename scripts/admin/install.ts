6093fad89180ec49880bc4d5370d2d65c11be420
opfx <opfx@opfx.org>
2020-08-08 03:17:51 -0400

    wip (admin) : install

--- scripts/admin/install.ts
+++ scripts/admin/install.ts
@@ -8,11 +8,35 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
+import { execSync } from 'child_process';
 import { logging } from '@angular-devkit/core';
 
 export default async function (args: any, log: logging.Logger) {
   console.log('running install tasks');
+
+  configureGitUser();
+  configureGitFlow();
+}
+
+function configureGitFlow(): void {
+  const branch = execSync('git rev-parse --abbrev-ref HEAD');
+  if (branch.toString().trim() === 'master') {
+    execSync(`git flow init -d`);
+  }
+}
+
+function configureGitUser(): void {
   const homedir = os.homedir();
-  const rootdir = path.join(__dirname, './../../', 'sample');
-  fs.mkdir(rootdir, { recursive: true }, () => {});
+  const gitUserConfigFile = path.join(homedir, '.gitconfig.opfx');
+  if (fs.existsSync(gitUserConfigFile)) {
+    const gitUserConfigFile = path.join(homedir, '.gitconfig.opfx');
+    const config = fs.readFileSync(gitUserConfigFile, 'utf8');
+
+    let matches = config.match(/name\s*=\s*([^\s]*)/);
+    const name = matches[1];
+    matches = config.match(/email\s*=\s*([^\s]*)/);
+    const email = matches[1];
+    execSync(`git config user.name ${name}`);
+    execSync(`git config user.email ${email}`);
+  }
 }
