940b0f9037444be4d4512909ce9079fb97a5307e
opfx <opfx@opfx.org>
2020-08-08 02:36:11 -0400

    wip : creating a sample dir

--- scripts/admin/install.ts
+++ scripts/admin/install.ts
@@ -5,9 +5,14 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
+import * as path from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
 import { logging } from '@angular-devkit/core';
 
 export default async function (args: any, log: logging.Logger) {
-  log.info('install...');
+  console.log('running install tasks');
+  const homedir = os.homedir();
+  const rootdir = path.join(__dirname, './../../', 'sample');
+  fs.mkdir(rootdir, { recursive: true }, () => {});
 }
