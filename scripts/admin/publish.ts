f8778f9a3b66965d6acfbd91eb92ae5e0900485f
opfx <opfx@opfx.org>
2020-08-08 01:50:14 -0400

    wip : admin publish

---
+++ scripts/admin/publish.ts
@@ -0,0 +1,77 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { spawnSync } from 'child_process';
+import { logging, tags } from '@angular-devkit/core';
+
+import { packages } from './../../lib/packages';
+
+import build from './build';
+import { Logger } from '@angular-devkit/core/src/logger';
+export interface PublishArgs {
+  tag?: string;
+  tagCheck?: boolean;
+  registry?: string;
+}
+export default async function (args: PublishArgs, log: logging.Logger) {
+  log.info('Publishing...');
+  let { tag } = args;
+  if (!tag) {
+    tag = 'next';
+  }
+
+  //   let registry = args.registry;
+  //   if()
+
+  await build({}, log.createChild('build'));
+
+  return Object.keys(packages)
+    .reduce((acc: Promise<void>, name: string) => {
+      const pkg = packages[name];
+      if (pkg.packageJson['private']) {
+        log.debug(`ignoring ${name} (private)`);
+        return acc;
+      }
+      return acc
+        .then(() => {
+          log.info(`publishing package ${name} with version => ${name}@`);
+
+          //   const publishArgs = ['publish', '--tag', tag, '--registry', registry];
+          const publishArgs = ['publish', '--tag', tag, '--dry-run'];
+          return exec('npm', publishArgs, { cwd: pkg.dist }, log);
+        })
+        .then((stdout: string) => {
+          log.info(stdout);
+        });
+    }, Promise.resolve())
+    .then(
+      () => {
+        log.info('done');
+      },
+      (err: Error) => log.fatal(err.message)
+    );
+}
+
+function exec(command: string, args: string[], opts: { cwd?: string }, log: logging.Logger) {
+  //   if (process.platform.startsWith('win')) {
+  //     args.unshift('/c', command);
+  //     command = 'cmd.exe';
+  //   }
+
+  log.info(`executing ${command} ${args.map((arg) => JSON.stringify(arg)).join(', ')}`);
+  const { status, error, stderr, stdout } = spawnSync(command, args, { ...opts, shell: true });
+
+  if (status !== 0) {
+    log.error(`Command failed: ${command} ${args.map((arg) => JSON.stringify(arg)).join(', ')}`);
+
+    const message = error ? error.message : `STDERR:\n${stderr}`;
+    log.error(`Error: ${message}`);
+    throw new Error(message);
+  } else {
+    return stdout.toString();
+  }
+}
