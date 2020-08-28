bc0241911128b5e51e7a5fd31a66eb2bc3d8d9e5
opfx <opfx@opfx.org>
2020-08-28 01:05:38 -0400

    feat (@wdtk/core/utils) : add string utilities

--- packages/core/util/index.ts
+++ packages/core/util/index.ts
@@ -1,7 +1,8 @@
+import * as strings from './strings';
 import * as tags from './tags';
 export * from './color';
 export * from './logging';
 export * from './file';
 export { SemVer } from 'semver';
 
-export { tags };
+export { strings, tags };
