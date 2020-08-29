7ba47c99cd0aa1e2f161f413c55362332406807f
opfx <opfx@opfx.org>
2020-08-29 12:44:32 -0400

    chore (@wdtk/core): removed obsolete code

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,2 @@
-export * from './lib/sample';
 export * from './lib/command';
+export * from './lib/json';
--- packages/core/src/lib/sample.ts
+++
@@ -1,5 +0,0 @@
-import { helloWorld } from 'packages/cli/src/lib';
-
-export function helloWorldMessage(): string {
-  return 'hello world from core';
-}
