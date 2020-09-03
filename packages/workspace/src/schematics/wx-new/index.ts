e2124f837a4a1dddc97e81034772e166b95861e6
opfx <opfx@opfx.org>
2020-09-02 20:36:53 -0400

    removed the wx-new schematic

--- packages/workspace/src/schematics/wx-new/index.ts
+++
@@ -1,9 +0,0 @@
-import { chain } from '@angular-devkit/schematics';
-import { Rule } from '@angular-devkit/schematics';
-
-import { Schema as WxNewOptions } from './schema';
-
-export default function (schema: WxNewOptions): Rule {
-  console.error('running wx new schematic');
-  return chain([]);
-}
