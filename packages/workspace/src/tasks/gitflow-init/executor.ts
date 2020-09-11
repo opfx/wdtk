ab657bb09697d3af7837ad2e0dd029775d690da4
opfx <opfx@opfx.org>
2020-09-11 16:13:39 -0400

    wip (tasks) : wip gitflow-init task

--- packages/workspace/src/tasks/gitflow-init/executor.ts
+++ packages/workspace/src/tasks/gitflow-init/executor.ts
@@ -1,8 +1,13 @@
-import { TaskExecutor } from '@angular-devkit/schematics';
+import * as Path from 'path';
+import { SchematicContext, TaskExecutor } from '@angular-devkit/schematics';
 import { Observable } from 'rxjs';
-export default function (opts): TaskExecutor<any> {
-  return (options: any) => {
+import { GitFlowInitTaskName, GitFlowInitOptions, GitFlowInitFactoryOptions } from './index';
+export default function (factoryOptions: GitFlowInitFactoryOptions = {}): TaskExecutor<GitFlowInitOptions> {
+  const rootDirectory = factoryOptions.rootDirectory || process.cwd();
+  return (options: GitFlowInitOptions, ctx: SchematicContext) => {
+    const cwd = Path.join(rootDirectory, options.workingDirectory || '');
     return new Observable((obs) => {
+      ctx.logger.debug(`Executing '${GitFlowInitTaskName}' task in '${cwd}'.`);
       obs.next();
       obs.complete();
     });
--- packages/workspace/src/tasks/gitflow-init/index.ts
+++ packages/workspace/src/tasks/gitflow-init/index.ts
@@ -1,2 +1,35 @@
-export * from './api';
-export * from './task';
+import { TaskConfiguration, TaskConfigurationGenerator } from '@angular-devkit/schematics';
+
+export const GitFlowInitTaskName = 'gitflow-init';
+export interface GitFlowInitFactoryOptions {
+  rootDirectory?: string;
+}
+export interface GitFlowInitOptions {
+  workingDirectory?: string;
+}
+
+export class GitFlowInitTask implements TaskConfigurationGenerator<GitFlowInitOptions> {
+  private workingDirectory: string;
+
+  constructor(workingDirectory?: string);
+  constructor(options: Partial<GitFlowInitOptions>);
+  constructor(options?: string | Partial<GitFlowInitOptions>) {
+    if (typeof options === 'string') {
+      this.workingDirectory = options;
+    }
+    if (typeof options === 'object') {
+      if (options.workingDirectory) {
+        this.workingDirectory = options.workingDirectory;
+      }
+    }
+  }
+
+  toConfiguration(): TaskConfiguration<GitFlowInitOptions> {
+    return {
+      name: GitFlowInitTaskName,
+      options: {
+        workingDirectory: this.workingDirectory,
+      },
+    };
+  }
+}
