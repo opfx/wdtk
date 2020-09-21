9f5b969b1f901294753d873ef778c2444323d9ce
opfx <opfx@opfx.org>
2020-09-21 10:44:50 -0400

    added the getJsonFileContent tet utility function

--- packages/core/testing/index.ts
+++ packages/core/testing/index.ts
@@ -1,4 +1,6 @@
 import { Tree } from '@angular-devkit/schematics';
+import { UnitTestTree } from '@angular-devkit/schematics/testing';
+import { JsonParseMode, parseJson } from '@wdtk/core';
 
 export * from './mock-builder-context';
 
@@ -8,4 +10,9 @@ export function createEmptyWorkspace(host: Tree = null): Tree {
   }
   host.create('/package.json', JSON.stringify({ name: 'empty', dependencies: {}, devDependencies: {} }));
   return host;
+}
+
+// tslint:disable-next-line: no-any
+export function getJsonFileContent(tree: UnitTestTree, path: string): any {
+  return parseJson(tree.readContent(path).toString(), JsonParseMode.Loose);
 }
