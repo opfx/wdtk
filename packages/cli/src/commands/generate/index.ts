59255b1315a401b2da2534b00434ca2dc4ac6f58
opfx <opfx@opfx.org>
2020-09-29 16:14:23 -0400

    wip (cli:generate) : ask which collection to use when the schematic name specified

--- packages/cli/src/commands/generate/index.ts
+++ packages/cli/src/commands/generate/index.ts
@@ -1,20 +1,67 @@
 import * as Inquirer from 'inquirer';
-import { Arguments, SchematicCommand } from '@wdtk/core';
+import { Arguments, SchematicCommand, SubCommandDescriptor } from '@wdtk/core';
+import { parseJsonSchemaToSubCommandDescriptor } from '@wdtk/core';
+import { strings } from '@wdtk/core/util';
 
 import { Schema as GenerateCommandOptions } from './schema';
 
+const availableCollectionNames: string[] = ['@wdtk/angular', '@wdtk/php'];
 export class GenerateCommand extends SchematicCommand<GenerateCommandOptions> {
   async initialize(options: GenerateCommandOptions & Arguments) {
+    await super.initialize(options);
     let [collectionName, schematicName] = await this.parseSchematicInfo(options);
-    if (!schematicName) {
-      const schematic = await this.determineSchematic();
-      const collection = await this.determineCollection(schematic);
-      [collectionName, schematicName] = [collection, schematic];
+    if (!(options.help === true || options.help === 'json' || options.help === 'JSON')) {
+      if (!schematicName) {
+        schematicName = await this.determineSchematic();
+      }
+      if (!collectionName) {
+        collectionName = await this.determineCollection(schematicName);
+      }
     }
-    this.collectionName = collectionName;
-    this.schematicName = schematicName;
+  }
+  async initializeA(options: GenerateCommandOptions & Arguments) {
+    let [collectionName, schematicName] = await this.parseSchematicInfo(options);
+    if (!(options.help === true || options.help === 'json' || options.help === 'JSON')) {
+      if (!schematicName) {
+        const schematic = await this.determineSchematic();
+        const collection = await this.determineCollection(schematic);
+        [collectionName, schematicName] = [collection, schematic];
+      }
+      this.collectionName = collectionName;
+      this.schematicName = schematicName;
+    }
 
     await super.initialize(options);
+
+    const subCommands: { [name: string]: SubCommandDescriptor } = {};
+
+    const collectionNames: string[] = ['@wdtk/angular', '@wdtk/php'];
+
+    // await collectionNames.forEach(async (collectionName) => {
+    for (const collectionName of collectionNames) {
+      const collection = this.getCollection(collectionName);
+
+      const schematicNames = collection.listSchematicNames();
+      schematicNames.sort();
+      for (const schematicName of schematicNames) {
+        const schematic = this.getSchematic(collection, schematicName, true);
+        let subCommand: SubCommandDescriptor;
+        if (schematic.description.schemaJson) {
+          subCommand = await parseJsonSchemaToSubCommandDescriptor(
+            schematicName,
+            schematic.description.path,
+            this.workflow.registry,
+            schematic.description.schemaJson
+          );
+          subCommands[`${collectionName}:${schematicName}`] = subCommand;
+        }
+      }
+    }
+    this.descriptor.options.forEach((option) => {
+      if (option.name === 'schematic') {
+        option.subcommands = subCommands;
+      }
+    });
   }
   async run(options: GenerateCommandOptions & Arguments): Promise<number | void> {
     if (!this.schematicName || !this.collectionName) {
@@ -31,11 +78,40 @@ export class GenerateCommand extends SchematicCommand<GenerateCommandOptions> {
     });
   }
 
-  private async determineCollection(schematic: string): Promise<string> {
+  private async determineCollection(schematicName: string): Promise<string> {
     return new Promise<string>((resolve) => {
-      Inquirer.prompt({ message: `What type of ${schematic}?`, name: 'collection', type: 'list', choices: [{ name: 'Angular', value: 'angular' }] }).then(
+      const choices: any[] = [];
+      const addChoice = (collectionName) => {
+        const shortCollectionName = collectionName.split('/')[1];
+        choices.push({ name: strings.classify(shortCollectionName), value: collectionName });
+      };
+      for (const collectionName of availableCollectionNames) {
+        const collection = this.getCollection(collectionName);
+
+        const schematicNames = collection.listSchematicNames();
+
+        // if the specified schematicName matches any of the long schematic names add it to
+        // the list of choices and continue with the next collection
+        if (schematicNames.some((name) => name === schematicName)) {
+          addChoice(collectionName);
+          continue;
+        }
+        // if the specified schematicName does not match any of the names it might match
+        // one of the aliases
+        for (const currentSchematicName of schematicNames) {
+          const schematic = this.getSchematic(collection, currentSchematicName, true);
+          if (schematic.description.hidden) {
+            continue;
+          }
+          if (schematic.description.aliases && schematic.description.aliases.some((alias) => alias === schematicName)) {
+            addChoice(collectionName);
+            schematicName = schematic.description.name;
+          }
+        }
+      }
+      Inquirer.prompt({ message: `What type of "${schematicName}" would you like to generate?`, name: 'collection', type: 'list', choices: choices }).then(
         (answer) => {
-          return resolve(`@wdtk/${answer.collection}`);
+          return resolve(answer.collection);
         }
       );
     });
@@ -50,11 +126,11 @@ export class GenerateCommand extends SchematicCommand<GenerateCommandOptions> {
   }
 
   private async parseSchematicInfo(options: { schematic?: string }): Promise<[string, string | undefined]> {
-    let collectionName = await this.getDefaultSchematicCollection();
+    let collectionName: string = undefined;
     let schematicName = options.schematic;
-
     if (schematicName && schematicName.includes(':')) {
-      [collectionName, schematicName] = schematicName.split(':', 2);
+      let collectionName: string;
+      [collectionName, schematicName] = schematicName.split(':');
     }
     return [collectionName, schematicName];
   }
--- packages/core/src/lib/command/index.ts
+++ packages/core/src/lib/command/index.ts
@@ -2,5 +2,6 @@ export * from './command';
 export * from './schematic-command';
 export * from './exceptions';
 export * from './runner';
+export * from './schema';
 export * from './types';
 export * from './workspace';
