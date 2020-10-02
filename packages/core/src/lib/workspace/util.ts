feda3d34198508f4e4a18a2e54420af4ab80937c
opfx <opfx@opfx.org>
2020-10-02 16:27:44 -0400

    wip (core) : added support to get the workspace definition from non schematic code (no tree available)

--- packages/core/src/lib/workspace/util.ts
+++ packages/core/src/lib/workspace/util.ts
@@ -1,11 +1,25 @@
+import { NodeJsSyncHost } from '@angular-devkit/core/node';
 import { Tree } from '@angular-devkit/schematics';
 import { workspaces } from '@angular-devkit/core';
 
+import { findUp } from '@wdtk/core/util';
+
 import { WorkspaceDefinition } from './types';
 
+const configFiles = ['.angular.json', 'angular.json'];
+const cachedWorkspaces = new Map<string, WorkspaceDefinition | null>();
+
 export function getWorkspaceDefinitionPath(tree: Tree) {
-  const configFiles = ['.angular.json', 'angular.json'];
+  // const configFiles = ['.angular.json', 'angular.json'];
   return configFiles.filter((path) => tree.exists(path))[0];
+}
+
+/**
+ * This is a local utility function
+ * should not be exported
+ */
+function localWorkspaceDefinitionPath(): string | null {
+  return findUp(configFiles, process.cwd()) || findUp(configFiles, __dirname);
 }
 
 /**
@@ -13,9 +27,34 @@ export function getWorkspaceDefinitionPath(tree: Tree) {
  * @param tree
  * @param path
  */
-export async function getWorkspaceDefinition(tree: Tree, path = '/'): Promise<WorkspaceDefinition> {
-  const host = createHost(tree);
+export async function getWorkspaceDefinition(): Promise<WorkspaceDefinition>;
+export async function getWorkspaceDefinition(tree: Tree): Promise<WorkspaceDefinition>;
+export async function getWorkspaceDefinition(tree: Tree, path: string): Promise<WorkspaceDefinition>;
+export async function getWorkspaceDefinition(tree: Tree | string = null, path = '/'): Promise<WorkspaceDefinition> {
+  let host;
+  if (!tree || typeof tree === 'string') {
+    host = workspaces.createWorkspaceHost(new NodeJsSyncHost());
+    if ((tree as string) === 'global') {
+      // just in case we want to use a global configuration as angular does
+    } else {
+      path = localWorkspaceDefinitionPath();
+    }
+  } else {
+    host = createHost(tree);
+  }
+
+  if (!path) {
+    return null;
+  }
+  const cached = cachedWorkspaces.get(path);
+  if (cached) {
+    return cached;
+  }
+
   const { workspace } = await workspaces.readWorkspace(path, host);
+
+  cachedWorkspaces.set(path, workspace);
+
   return workspace;
 }
 
