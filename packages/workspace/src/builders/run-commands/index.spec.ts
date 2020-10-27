7de0b45a638e6344e0072e83e4d5b18099e38fba
opfx <opfx@opfx.org>
2020-10-27 09:11:18 -0400

    wip (workspace): added run-commands builder

---
+++ packages/workspace/src/builders/run-commands/index.spec.ts
@@ -0,0 +1,192 @@
+import { Architect } from '@angular-devkit/architect';
+
+import { createMockArchitect } from '@wdtk/core/testing';
+
+import { readFileSync, existsSync, unlinkSync, exists } from 'fs';
+import * as Os from 'os';
+import * as Path from 'path';
+
+const builder = '@wdtk/workspace:run-commands';
+
+function readFile(f: string) {
+  return readFileSync(f).toString().replace(/\s/g, '');
+}
+
+describe(`run-commands`, () => {
+  let architect: Architect;
+  beforeEach(async () => {
+    architect = await createMockArchitect('@wdtk/workspace');
+  });
+
+  it(`should run one command`, async () => {
+    const f = Path.join(Os.tmpdir(), 'run-commands.txt');
+    const run = await architect.scheduleBuilder(builder, { command: `echo 1 > ${f}` });
+    const result = await run.result;
+    expect(result.success).toBe(true);
+    expect(readFile(f)).toEqual('1');
+  });
+
+  it(`should interpolate provide --args`, async () => {
+    const f = Path.join(Os.tmpdir(), 'run-commands.txt');
+    const run = await architect.scheduleBuilder(builder, {
+      command: `echo {args.key} > ${f}`,
+      args: '--key=123',
+    });
+    const result = await run.result;
+    expect(result.success).toBe(true);
+    expect(readFile(f)).toEqual('123');
+  });
+
+  it(`should interpolate all unknown args as if they were --args`, async () => {
+    const f = Path.join(Os.tmpdir(), 'run-commands.txt');
+    const run = await architect.scheduleBuilder(builder, {
+      command: `echo {args.key} > ${f}`,
+      key: '123',
+    });
+    const result = await run.result;
+    expect(result.success).toBe(true);
+    expect(readFile(f)).toEqual('123');
+  });
+
+  it(`should add all args to the command if no interpolation in the command`, async () => {
+    const spawn = spyOn(require('child_process'), 'spawn').and.callThrough();
+
+    const run = await architect.scheduleBuilder(builder, {
+      command: `echo`,
+      a: 123,
+      b: 456,
+    });
+    const result = await run.result;
+    expect(spawn).toHaveBeenCalledWith(`echo --a=123 --b=456`, [], expect.objectContaining({ shell: true }));
+  });
+
+  it(`should forward args by default when using commands (plural)`, async () => {
+    const spawn = spyOn(require('child_process'), 'spawn').and.callThrough();
+
+    const run = await architect.scheduleBuilder(builder, {
+      commands: [{ command: `echo` }],
+      a: 123,
+      b: 456,
+    });
+    const result = await run.result;
+    expect(spawn).toHaveBeenCalledWith(`echo --a=123 --b=456`, [], expect.objectContaining({ shell: true }));
+  });
+
+  it(`should forward args when forwardAllArgs is set to true`, async () => {
+    const spawn = spyOn(require('child_process'), 'spawn').and.callThrough();
+
+    const run = await architect.scheduleBuilder(builder, {
+      commands: [{ command: `echo`, forwardAllArgs: true }],
+      a: 123,
+      b: 456,
+    });
+    const result = await run.result;
+    expect(spawn).toHaveBeenCalledWith(`echo --a=123 --b=456`, [], expect.objectContaining({ shell: true }));
+  });
+
+  it(`should not forward args when forwardAllArgs is set to false`, async () => {
+    const spawn = spyOn(require('child_process'), 'spawn').and.callThrough();
+
+    const run = await architect.scheduleBuilder(builder, {
+      commands: [{ command: `echo`, forwardAllArgs: false }],
+      a: 123,
+      b: 456,
+    });
+    const result = await run.result;
+    expect(spawn).toHaveBeenCalledWith(`echo`, [], expect.objectContaining({ shell: true }));
+  });
+
+  it(`should throw when invalid args`, async () => {
+    try {
+      const run = await architect.scheduleBuilder(builder, {
+        command: `echo {args.key}`,
+        args: 'key=value',
+      });
+      await run.result;
+    } catch (e) {
+      expect(e.message).toEqual('Invalid args: key=value');
+    }
+  });
+
+  it(`should run commands serially`, async () => {
+    jest.setTimeout(30000);
+    const f = Path.join(Os.tmpdir(), 'run-commands.serially.txt');
+
+    if (existsSync(f)) {
+      unlinkSync(f);
+    }
+    let sleepCmd = 'sleep 1';
+    if (Os.platform() === 'win32') {
+      sleepCmd = 'ping -n 10 127.0.0.1';
+    }
+    const run = await architect.scheduleBuilder(builder, {
+      // commands: [`${sleepCmd} && echo 1 >> ${f}`, `echo 2 >> ${f}`],
+      commands: [`${sleepCmd} && echo 1 >> ${f}`, `echo 2 >> ${f}`],
+      parallel: false,
+      verbose: true,
+    });
+    const result = await run.result;
+    expect(result.success).toBe(true);
+    expect(readFile(f)).toEqual('12');
+  });
+
+  it(`should run command in parallel`, async () => {
+    const f = Path.join(Os.tmpdir(), 'run-commands.txt');
+
+    if (existsSync(f)) {
+      unlinkSync(f);
+    }
+
+    const run = await architect.scheduleBuilder(builder, {
+      commands: [{ command: `echo 1 >> ${f}` }, { command: `echo 2 >> ${f}` }],
+      parallel: true,
+    });
+    const result = await run.result;
+    expect(result.success).toBe(true);
+    const content = readFile(f);
+    expect(content).toContain('1');
+    expect(content).toContain('2');
+  });
+
+  it(`should stop execution when a command fails`, async () => {
+    const f = Path.join(Os.tmpdir(), 'run-commands.txt');
+
+    if (existsSync(f)) {
+      unlinkSync(f);
+    }
+
+    const run = await architect.scheduleBuilder(builder, {
+      commands: [
+        { command: `echo 1 >> ${f}  && echox` }, //
+        { command: `echo 2 >> ${f}` },
+      ],
+      parallel: true,
+    });
+    const result = await run.result;
+
+    expect(result.success).toBe(false);
+    expect(readFile(f)).toEqual('1');
+  });
+
+  it(`run the command in the specified working directory`, async () => {
+    const f = Path.join(Os.tmpdir(), 'run-commands.cwd.txt');
+    let cmd = 'pwd';
+    if (Os.platform() === 'win32') {
+      cmd = 'echo %cd%';
+    }
+    const runA = await architect.scheduleBuilder(builder, {
+      commands: [{ command: `${cmd} > ${f}` }],
+    });
+    const resultA = await runA.result;
+    expect(resultA.success).toBe(true);
+    expect(readFile(f)).not.toContain('projects');
+
+    const runB = await architect.scheduleBuilder(builder, {
+      commands: [{ command: `${cmd} > ${f}` }],
+      cwd: 'projects',
+    });
+    const resultB = await runB.result;
+    expect(resultB.success).toBe(true);
+    expect(readFile(f)).toContain('projects');
+  });
+});
---
+++ packages/workspace/src/builders/run-commands/index.ts
@@ -0,0 +1,179 @@
+import { BuilderContext, BuilderOutput } from '@angular-devkit/architect';
+import { createBuilder } from '@angular-devkit/architect';
+import { JsonObject } from '@angular-devkit/core';
+import { normalize, isAbsolute, join, getSystemPath } from '@angular-devkit/core';
+
+import { SpawnOptions } from 'child_process';
+import { spawn } from 'child_process';
+
+import { Observable, Subscription } from 'rxjs';
+import { of, from, forkJoin } from 'rxjs';
+import { concatMap, switchMap, map, mergeMap } from 'rxjs/operators';
+
+import { Schema } from './schema';
+import { CommandElement, CommandClass } from './schema';
+
+export interface RunCommandsBuilderOptions extends Schema {
+  args?: string;
+  parsedArgs: { [k: string]: any };
+}
+
+export default createBuilder<RunCommandsBuilderOptions & JsonObject>(execute);
+
+const propKeys = getPropertyKeys();
+
+function execute(opts: RunCommandsBuilderOptions & JsonObject, ctx: BuilderContext): Observable<BuilderOutput> {
+  return from(normalizeOptions(opts, ctx)).pipe(switchMap((opts) => runCommands(opts, opts.cwd, ctx)));
+}
+
+function runCommands(opts: RunCommandsBuilderOptions, cwd: string, ctx: BuilderContext): Observable<BuilderOutput> {
+  let subscription: Subscription;
+  const commands = from(opts.commands).pipe(
+    concatMap((command) => {
+      return runCommand(command, opts.cwd, ctx);
+    })
+  );
+
+  return new Observable((observer) => {
+    subscription = commands.subscribe({
+      next: (v) => {},
+      complete: () => {
+        observer.next({ success: true });
+        observer.complete();
+      },
+      error: (e) => {
+        observer.next({ success: false });
+        observer.complete();
+      },
+    });
+
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  });
+}
+
+function runCommandsD(opts: RunCommandsBuilderOptions, cwd: string, ctx: BuilderContext): Observable<BuilderOutput> {
+  return from(opts.commands).pipe(
+    // mergeMap((command) => {
+    concatMap((command) => {
+      return runCommand(command, cwd, ctx);
+    }),
+    map((result) => {
+      return result;
+    })
+  );
+}
+
+function runCommand(opts, cwd: string, ctx: BuilderContext): Observable<BuilderOutput> {
+  const spawnOpts: SpawnOptions = {
+    cwd: cwd || process.cwd(),
+    shell: true,
+    stdio: 'inherit',
+  };
+  const spawnArgs = [];
+  // if (!opts.args) {
+  //   spawnArgs = opts.args;
+  // }
+  return new Observable<BuilderOutput>((observer) => {
+    ctx.logger.debug(`executing ${opts.command} with args ${JSON.stringify(opts.args)} in '${spawnOpts.cwd}' directory`);
+    const cmdProcess = spawn(opts.command, spawnArgs, spawnOpts);
+
+    const killCmdProcess = () => {
+      if (cmdProcess && cmdProcess.pid) {
+        cmdProcess.kill();
+      }
+    };
+
+    // handle process exit
+    const handleCmdProcessExit = (code?: number) => {
+      let output: BuilderOutput = { success: true };
+      killCmdProcess();
+      if (code && code !== 0) {
+        // output = { success: false, error: 'Build failed. See above for details.' };
+        observer.error();
+        return;
+      }
+      observer.next(output);
+      observer.complete();
+    };
+
+    cmdProcess.on('exit', handleCmdProcessExit);
+    cmdProcess.on('SIGINT', handleCmdProcessExit);
+    cmdProcess.on('uncaughtException', handleCmdProcessExit);
+
+    // teardown logic
+    return killCmdProcess;
+  });
+}
+
+async function normalizeOptions(opts: RunCommandsBuilderOptions, ctx: BuilderContext): Promise<any> {
+  opts.parsedArgs = parseArgs(opts);
+
+  if (opts.command) {
+    opts.commands = [{ command: opts.command }];
+    opts.parallel = false;
+  } else {
+    (<any>opts).commands = opts.commands.map((c) => {
+      return typeof c === 'string' ? { command: c } : c;
+    });
+  }
+  opts.commands.forEach((c: CommandClass) => {
+    c.command = normalizeCommand(c.command, opts.parsedArgs, c.forwardAllArgs ?? true);
+  });
+
+  if (opts.cwd) {
+    let cwd = normalize(opts.cwd);
+    if (!isAbsolute(cwd)) {
+      cwd = join(normalize(ctx.workspaceRoot), cwd);
+    }
+    opts.cwd = getSystemPath(cwd);
+  }
+
+  return opts;
+}
+
+function normalizeCommand(command: string, args: { [key: string]: string }, forwardAllArgs: boolean) {
+  if (command.indexOf('{args.') > -1) {
+    const regex = /{args\.([^}]+)}/g;
+    return command.replace(regex, (_, group: string) => args[group]);
+  } else if (Object.keys(args).length > 0 && forwardAllArgs) {
+    const stringifiedArgs = Object.keys(args)
+      .map((a) => `--${a}=${args[a]}`)
+      .join(' ');
+    return `${command} ${stringifiedArgs}`;
+  } else {
+    return command;
+  }
+}
+
+function parseArgs(opts: RunCommandsBuilderOptions) {
+  const args = opts.args;
+  if (!args) {
+    const unknownOptionsTreatedAsArgs = Object.keys(opts)
+      .filter((p) => propKeys.indexOf(p) === -1)
+      .reduce((m, c) => ((m[c] = opts[c]), m), {});
+    return unknownOptionsTreatedAsArgs;
+  }
+  return args
+    .split(' ')
+    .map((t) => t.trim())
+    .reduce((m, c) => {
+      if (!c.startsWith('--')) {
+        throw new Error(`Invalid args: ${args}`);
+      }
+      const [key, value] = c.substring(2).split('=');
+      if (!key || !value) {
+        throw new Error(`Invalid args: ${args}`);
+      }
+      m[key] = value;
+      return m;
+    }, {});
+}
+
+function getPropertyKeys() {
+  const schema = require('./schema.json');
+  return Object.keys(schema.properties);
+}
