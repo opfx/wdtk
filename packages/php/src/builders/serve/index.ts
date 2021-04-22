8ac2c0180f4e142d3a91e312dbfb6decabfaa823
opfx <opfx@opfx.org>
2021-04-22 09:46:30 -0400

    refactor (php): build app before serve

--- packages/php/src/builders/serve/index.ts
+++ packages/php/src/builders/serve/index.ts
@@ -1,10 +1,10 @@
 import { BuilderContext, BuilderOutput } from '@angular-devkit/architect';
-import { createBuilder } from '@angular-devkit/architect';
+import { createBuilder, targetFromTargetString } from '@angular-devkit/architect';
 import { JsonObject } from '@angular-devkit/core';
 
-import { spawn, SpawnOptions } from 'child_process';
-import * as Path from 'path';
-import * as File from 'fs';
+import { spawn } from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
 
 import { Observable } from 'rxjs';
 import { from, of } from 'rxjs';
@@ -41,8 +41,35 @@ export function execute(opts: ServeBuilderOptions & JsonObject, ctx: BuilderCont
 
 function runBuild(opts: ServeBuilderOptions, ctx: BuilderContext): Observable<BuilderOutput> {
   return new Observable<BuilderOutput>((obs) => {
-    obs.next({ success: true });
-    obs.complete();
+    const doBuild = async () => {
+      const buildTarget = targetFromTargetString(opts.buildTarget);
+      const buildOptions = await ctx.getTargetOptions(buildTarget);
+
+      const buildTargetRun = await ctx.scheduleTarget(buildTarget, { ...buildOptions, watch: false });
+
+      try {
+        // throw new Error('Unknown error');
+
+        return await buildTargetRun.result;
+      } finally {
+        await buildTargetRun.stop();
+      }
+    };
+
+    doBuild()
+      .then((result: any) => {
+        if (result.errors && result.errors.length > 0) {
+          obs.next({ success: false });
+        } else {
+          obs.next({ success: true });
+        }
+      })
+      .catch((e) => {
+        obs.next({ success: false, error: e.message });
+      })
+      .finally(() => {
+        obs.complete();
+      });
   });
 }
 
@@ -121,9 +148,9 @@ function getPhpArgs(opts: ServeBuilderOptions): string[] {
 async function initialize(opts: ServeBuilderOptions, ctx: BuilderContext): Promise<any> {
   // create the xdebug output dir
   if (opts.debug.outputDir && opts.debug.mode !== 'off') {
-    if (!File.existsSync(opts.debug.outputDir)) {
+    if (!fs.existsSync(opts.debug.outputDir)) {
       ctx.logger.debug(` ∙ creating xdebug output directory '${opts.debug.outputDir}'`);
-      File.mkdirSync(opts.debug.outputDir, { recursive: true });
+      fs.mkdirSync(opts.debug.outputDir, { recursive: true });
     }
   }
 
@@ -131,16 +158,16 @@ async function initialize(opts: ServeBuilderOptions, ctx: BuilderContext): Promi
 }
 
 async function normalizeOptions(opts: ServeBuilderOptions, ctx: BuilderContext, projectMetadata: any): Promise<any> {
-  const projectRoot = Path.resolve(ctx.workspaceRoot, projectMetadata.root);
+  const projectRoot = path.resolve(ctx.workspaceRoot, projectMetadata.root);
 
-  const mainPath = Path.resolve(ctx.workspaceRoot, opts.main);
-  const docRoot = Path.dirname(mainPath);
-  const index = Path.basename(opts.main);
+  const mainPath = path.resolve(ctx.workspaceRoot, opts.main);
+  const docRoot = path.dirname(mainPath);
+  const index = path.basename(opts.main);
   if (opts.debug.enabled) {
     if (opts.debug.outputDir) {
-      opts.debug.outputDir = Path.resolve(ctx.workspaceRoot, opts.debug.outputDir);
+      opts.debug.outputDir = path.resolve(ctx.workspaceRoot, opts.debug.outputDir);
     } else {
-      opts.debug.outputDir = Path.join(projectRoot, '.xdebug');
+      opts.debug.outputDir = path.join(projectRoot, '.xdebug');
       ctx.logger.debug(` ∙ xdebug output directory is not specified; using default '${opts.debug.outputDir}'`);
     }
   } else {
